6c81a2a9a9195f451fd9ac244ce8027e835f91c4
Ivan Kozelskikh <johndemigod3318@gmail.com>
2019-04-03 21:22:12 +0500

    minimum chat logger

--- Main.go
+++ Main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/gempir/go-twitch-irc"
+)
+
+func main() {
+	client := twitch.NewClient("vanya109", "oauth:kic4lme1k221w4c6soyrppnwros6b0")
+	client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
+		fmt.Println(user.DisplayName, message.Text)
+	})
+
+	client.Join("vanya83")
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
 }
