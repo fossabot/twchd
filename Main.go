6a3859a55fe465e3aca345885eb88bc963b44ef0
Ivan Kozelskikh <johndemigod3318@gmail.com>
2019-04-03 22:17:56 +0500

    added config parser

--- Main.go
+++ Main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
+	"log"
 
 	"github.com/gempir/go-twitch-irc"
+	"github.com/go-yaml/yaml"
 )
 
-func main() {
-	client := twitch.NewClient("vanya109", "oauth:kic4lme1k221w4c6soyrppnwros6b0")
-	client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
-		fmt.Println(user.DisplayName, message.Text)
-	})
-
-	client.Join("vanya83")
-
-	err := client.Connect()
+func check(err error) {
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
+}
+
+// BotConfig struct represent config from file
+type BotConfig struct {
+	AccountName  string   `yaml:"account_name"`
+	AccountToken string   `yaml:"account_token"`
+	AccountsList []string `yaml:"join_to"`
+}
+
+// ConfigParser takes config file and return BotConfig struct
+func ConfigParser(filename string) (config *BotConfig) {
+	rawConfig, err := ioutil.ReadFile(filename)
+	check(err)
+
+	config = new(BotConfig)
+	check(yaml.Unmarshal(rawConfig, config))
+	return
+}
+
+func main() {
+	config := ConfigParser("config.yml")
+
+	client := twitch.NewClient(config.AccountName, config.AccountToken)
+	// client.OnNewMessage(func(channel string, user twitch.User, message twitch.Message) {
+	// 	fmt.Println(user.DisplayName, message.Text)
+	// })
+
+	// client.Join("vanya83")
+
+	// err = client.Connect()
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
